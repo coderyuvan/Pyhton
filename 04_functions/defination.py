9c9bb178686558bd8af7a8b1c3b14417aa317679
Yuvan Grover <1095yuvangrover@gmail.com>
2025-08-08 02:01:37 +0530

    Learned how to write functions in python

---
+++ 04_functions/defination.py
@@ -0,0 +1,5 @@
+def calcSquare(n):
+    return n ** 2
+
+print(calcSquare(5))
+print(calcSquare(10)) 
---
+++ 04_functions/multiParam.py
@@ -0,0 +1,6 @@
+def calsum(a,b):
+    return a+b
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Sum is: ", calsum(a,b))
